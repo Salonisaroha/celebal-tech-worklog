e179ee47992af6250be5eaab617a01e109022c76
Saloni Saroha <salonisaroha5@gmail.com>
2025-06-29 10:33:15 +0530

    Add in-memory product data model

---
+++ product-api/models/productModel.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    id: '1',
+    name: 'Laptop',
+    price: 999,
+    description: 'A high-performance laptop.'
+  },
+  {
+    id: '2',
+    name: 'Smartphone',
+    price: 599,
+    description: 'A feature-rich smartphone.'
+  }
+];
+
+module.exports = products;
